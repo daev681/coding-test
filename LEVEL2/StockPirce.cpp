d1e5781a6d1c3bf0f99797e4c8509093a4d12daf
daev <54939319+daev681@users.noreply.github.com>
2023-10-22 09:43:29 +0900

    [Stack] 주식 가격 풀이 완료

--- LEVEL2/StockPirce.cpp
+++ LEVEL2/StockPirce.cpp
@@ -7,15 +7,16 @@ using namespace std;
 
 vector<int> solution(vector<int> prices) {
     vector<int> answer;
-    
-    queue<pair<int,int>> q;
-    
+
     for(int i = 0; i < prices.size(); i++){
         int value = 0;
-        for(int j = i; j < prices.size(); j++){
-           if(prices[i] < prices[j]){
+        for(int j = i + 1; j < prices.size(); j++){
+           if(prices[i] <= prices[j]){
                value++;
-           }
+           }else {
+                value++;
+                break; // 현재 가격보다 작아지면 루프 종료
+            }
         }
         answer.push_back(value);
     }
