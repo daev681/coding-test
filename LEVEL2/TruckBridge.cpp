18e7dcce5709d88d9cc3133331af6d82a87f9c54
daev681 <wow1186@naver.com>
2023-12-28 18:51:41 +0900

    완전 탐색 추가

--- LEVEL2/TruckBridge.cpp
+++ LEVEL2/TruckBridge.cpp
@@ -13,15 +13,15 @@ int main() {
     int bridge_length1 = 2;
     int weight1 = 10;
     vector<int> truck_weights1 = { 7, 4, 5, 6 };
-    int result1 = solution(bridge_length1, weight1, truck_weights1);
-    cout << "Test case 1 result: " << result1 << endl;
+  //  int result1 = solution(bridge_length1, weight1, truck_weights1);
+  //  cout << "Test case 1 result: " << result1 << endl;
 
     // Test case 2
     int bridge_length2 = 100;
     int weight2 = 100;
     vector<int> truck_weights2 = { 10 };
-    int result2 = solution(bridge_length2, weight2, truck_weights2);
-    cout << "Test case 2 result: " << result2 << endl;
+  //  int result2 = solution(bridge_length2, weight2, truck_weights2);
+  //  cout << "Test case 2 result: " << result2 << endl;
 
     // Test case 3
     int bridge_length3 = 100;
@@ -33,14 +33,6 @@ int main() {
     return 0;
 }
 
-
-
-using namespace std;
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <queue>
-using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int answer = 0;
@@ -55,7 +47,7 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
     while (!waiting.empty() || !running.empty()) {
         int wait_front = waiting.empty() ? 0 : waiting.front();
 
-        if (running.size() != 0 && running.size() != bridge_length && wait_front != 0) {
+        if (running.size() < bridge_length  && wait_front != 0) {
             int check = running_weight + wait_front;
             if (check >= weight) {
                 running_weight -= running.front();
