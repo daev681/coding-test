26470902e2d68f4eee96e7243b43b67bbfea0d2a
daev <54939319+daev681@users.noreply.github.com>
2024-01-23 16:26:42 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 84 ms, Memory: 8840 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+void dfs(int vertex, vector<vector<int>>& graph, vector<bool>& visited, vector<int>& sequence) {
+    static int order = 1;
+    visited[vertex] = true;
+    sequence[vertex] = order++;
+
+    for (int i = 0; i < graph[vertex].size(); i++) {
+        int adjacentVertex = graph[vertex][i];
+        if (!visited[adjacentVertex]) {
+            dfs(adjacentVertex, graph, visited, sequence);
+        }
+    }
+}
+
+void bfs(int vertex, vector<vector<int>>& graph, vector<bool>& visited, vector<int>& sequence) {
+    static int order = 1;
+    queue<int> q;
+
+    visited[vertex] = true;
+    sequence[vertex] = order++;
+    q.push(vertex);
+
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for (int v : graph[u]) {
+            if (!visited[v]) {
+                visited[v] = true;
+                sequence[v] = order++;
+                q.push(v);
+            }
+        }
+    }
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m, r;
+    cin >> n >> m >> r;
+    const int MAX = n + 1;
+    vector<vector<int>> graph(MAX);
+    vector<bool> visited(MAX, false);
+    vector<int> sequence(MAX);
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+
+    for (int i = 1; i <= n; i++) {
+        sort(graph[i].begin(), graph[i].end() , greater<int>());
+    }
+
+    bfs(r, graph, visited, sequence);
+
+    for (int i = 1; i <= n; i++) {
+        cout << sequence[i] << "\n";
+    }
+
+    return 0;
+}
